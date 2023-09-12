eb8995514959fb2fe7ca6a7011065bd9897ada27
rustygloves <dkbuntzen@gmail.com>
2023-09-12 17:09:29 +0200

    initial action implementation

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # pr-list-action
 A GitHub action to add a list of merged Pull Requests in a branch to a Pull Request
+
+## Action
+```yaml
+name: Update PR Body
+on:
+  pull_request:
+    branches: ["main"]
+
+jobs:
+  update-pr-body:
+    permissions: write-all
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v3
+      - name: Update PR Body
+        uses: sequel-technologies/pr-list-action@v1.0
+        with:
+          github-token: ${{ secrets.GITHUB_TOKEN }}
+          owner: sequel-technologies
+          repo: ${{ github.repository }}
+          head: ${{ github.head_ref }}
+          base: ${{ github.base_ref }}
+```
+
+## Building
+
+1. Install `vercel/ncc` by running this command in your terminal
+    ```shell
+    npm i -g @vercel/ncc
+    ```
+
+1. Compile the index.js file
+    ```shell
+    ncc build src/index.js --license licenses.txt
+    ```
---
+++ src/index.js
@@ -0,0 +1,50 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const main = async () => {
+  try {
+    const token = core.getInput("github-token", { required: true });
+    const owner = core.getInput("owner", { required: true });
+    const repo = core.getInput("repo", { required: true });
+    const head = core.getInput("head", { required: true });
+    const base = core.getInput("base", { required: true });
+
+    const octokit = new github.getOctokit(token);
+
+    const { data: comparison } = await octokit.rest.repos.compareCommits({
+      owner,
+      repo,
+      base,
+      head,
+    });
+
+    const uniqueCommits = comparison.commits.map(commit => commit.sha);
+
+    const pullRequestsPromises = uniqueCommits.map(commitSha =>
+      octokit.rest.repos.listPullRequestsAssociatedWithCommit({
+        owner,
+        repo,
+        commit_sha: commitSha,
+      }).catch(error => {
+        console.error(`Error fetching PR for commit ${commitSha}:`, error.message);
+        return null;
+      })
+    );
+
+    const pullRequestsResponses = await Promise.all(pullRequestsPromises);
+
+    const pullRequests = pullRequestsResponses.reduce((acc, response) => {
+      if (response && response.data.length > 0) {
+        acc.push(response.data[0]); // Assuming each commit is associated with at most one pull request
+      }
+      return acc;
+    }, []);
+
+    core.setOutput("pull-requests", JSON.stringify(pullRequests));
+  } catch (error) {
+    console.log("error");
+    core.setFailed(error.message);
+  }
+};
+
+main();
