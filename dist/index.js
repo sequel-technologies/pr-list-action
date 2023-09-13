6c2f320b138b0006d971f9a4bda62aa565e509c0
rustygloves <dkbuntzen@gmail.com>
2023-09-13 12:44:11 +0200

    update build

--- dist/index.js
+++ dist/index.js
@@ -9823,7 +9823,7 @@ const main = async () => {
 
     const { data: comparison } = await octokit.rest.repos.compareCommits({
       owner,
-      repo,
+      repo: repo.split("/")[1],
       base,
       head,
     });
